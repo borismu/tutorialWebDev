2246a693b42e46740db514a66aeb1053c177e2c6
burman <urmanbm@gmail.com>
2016-05-19 01:54:11 +0300

    fixes

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
-	title = models.CharField(max_length=50)
-	text = models.TextField(max_length=200)
-	added_at = models.DateTimeField()
-	rating = models.FloatField()
-	author = models.ForeignKey(User, related_name="writtenqs")
-	likes = models.ManyToManyField(User, related_name="likedqs")
+	title = models.CharField(max_length=50, blank=True)
+	text = models.TextField(max_length=200, blank=True)
+	added_at = models.DateTimeField(blank=True)
+	rating = models.IntegerField(blank=True)
+	author = models.ForeignKey(User, related_name="writtenqs", blank=True)
+	likes = models.ManyToManyField(User, related_name="likedqs", blank=True)
 
 class Answer(models.Model):
 	text = models.TextField(max_length=200)
