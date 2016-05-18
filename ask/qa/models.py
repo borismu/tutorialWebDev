2f93170b97803aded7d2e02154689c2510c5b297
burman <urmanbm@gmail.com>
2016-05-19 01:38:45 +0300

    new sql

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
-	title = models.CharField()
-	text = models.TextField()
+	title = models.CharField(max_length=50)
+	text = models.TextField(max_length=200)
 	added_at = models.DateTimeField()
 	rating = models.FloatField()
-	author = models.ForeignKey(User)
-	likes = models.ManyToManyField(User)
+	author = models.ForeignKey(User, related_name="writtenqs")
+	likes = models.ManyToManyField(User, related_name="likedqs")
 
 class Answer(models.Model):
-	text = models.CharField()
+	text = models.TextField(max_length=200)
 	added_at = models.DateTimeField()
 	author = models.ForeignKey(User)
 	question = models.ForeignKey(Question)
--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@ gunicorn hello:app -c='/home/box/web/etc/hello.py'
 cd ask
 gunicorn ask.wsgi:application -b=0.0.0.0:8000
 
+sudo /etc/init.d/mysql start
 mysql -u root -e "create database askdata character set utf8"
 mysql -u root -e "create user 'askuser' identified by '123'"
 mysql -u root -e "GRANT ALL ON askdata.* TO 'askuser'"
